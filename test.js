438c6e4f557f30e8c1a751b990edc2b87f84ea79
Steven Vandevelde <icid.asset@gmail.com>
2016-08-31 12:17:06 +0200

    0.3.1 - Fix limiter in runWithMessageAndLimiter, can now handle dot-slash paths

--- test.js
+++ test.js
@@ -22,6 +22,25 @@ test('exec', async t => {
 });
 
 
+test('exec with dot-slash path', async t => {
+  return exec([
+    (attr) => runWithMessageAndLimiter
+      ('Reading')
+      (attr.priv.changedPath)
+      (read)
+      ('./test/fixtures/hi.txt', attr.priv.root)
+
+  ], {
+    rootDirectory: __dirname,
+
+  }).then(
+    results => t.is(results[0][0].content, 'Hi!\n'),
+    handleError(t)
+
+  );
+});
+
+
 /*
 
   Utilities
